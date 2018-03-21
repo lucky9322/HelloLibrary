1a89e5b5e51b19190772ef2520727144e6b12005
lucky9322 <86431843@qq.com>
2018-03-21 13:28:18 +0800

    init
    Signed-off-by: lucky9322 <86431843@qq.com>

---
+++ library/src/main/java/com/jc_zhangdd/library/CustomUtil.java
@@ -0,0 +1,12 @@
+package com.jc_zhangdd.library;
+
+/**
+ * Project: Hellolibrary
+ * Created by Zdd on 2018/3/21.
+ */
+
+public class CustomUtil {
+    public static String getMyHero(){
+        return "你好，百度";
+    }
+}
